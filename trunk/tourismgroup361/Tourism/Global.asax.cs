e49aeab4f704f612e4b049058de940672cff1dc7
AlexanderKavokin@gmail.com <AlexanderKavokin@gmail.com@24201a40-1023-b425-d464-46dea2f49cf8>
2013-03-28 19:14:35 +0000

    возвращение к варианту "я нажал не туда, и оно сломалось!!111"=) Ну и с маршрутом разобрался, но не подключено. Мне требуются данные на вход. Андрей, именно от Вас!

--- trunk/tourismgroup361/Tourism/Global.asax.cs
+++ trunk/tourismgroup361/Tourism/Global.asax.cs
@@ -60,7 +60,7 @@ namespace Tourism
                         break;
 
                     default:
-                        viewResult.ViewName = "Error";
+                        //viewResult.ViewName = "Error";
                         break;
                 }
             }
